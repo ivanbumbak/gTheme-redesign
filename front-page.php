1421987b51365d2bba6dc6c716e52a11f34431d0
Ivan Bumbak <bumbak.ivan@gmail.com>
2020-06-27 11:37:07 +0200

    adding pages

--- front-page.php
+++ front-page.php
@@ -54,4 +54,38 @@
     </div>
 </div>
 
+<div class="section-nutshell">
+    <div class="container">
+        <div class="wrapper-nutshell">
+            <div class="nutshell-third">
+                <div class="nutshell-icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <p>Gambi d.o.o. je privatna tvrtka osnovana 1991. godine. Osnovna djelatnost bila je prodaja mjerila mase i vaga i registar blagajni.
+                S vremenom proširuje svoju djelatnost i danas izvodi instalacije rashladnih sistema, elektro i strojarskih instalacija, 
+                te na informatički inženjering.</p>
+                <a href="<?php the_permalink(13); ?>">Saznaj više</a>
+            </div>
+
+            <div class="nutshell-third">
+                <div class="nutshell-icon">
+                    <i class="fas fa-cog"></i>
+                </div>
+                <p>Prodaja, servis i montaža rashladnih sistema, elektro i strojarske instalacije. Servis, prodaja i baždaranje
+                mjerila mase, vage i utega. Instalacija nadzornog sustava. Servis i prodaja informatičke opreme, projektiranje i instalacija računalnih mreža.</p>
+                <a href="<?php the_permalink(3); ?>">Saznaj više</a>
+            </div>
+
+            <div class="nutshell-third">
+                <div class="nutshell-icon">
+                    <i class="fas fa-building"></i>
+                </div>
+                <p>Sjedište i servis tvrtke smješteno je u Bilicama, na adresi Novo Naselje 28 u neposrednoj blizini autokuće Mazda, pokraj biličkog spomenika. 
+                U sklopu servisa uključena je i veleprodaja tvrtke.</p>
+                <a href="<?php the_permalink(22); ?>">Saznaj više</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
---
+++ page-offer.php
@@ -0,0 +1,28 @@
+<?php
+
+get_header(); ?>
+
+    <?php
+
+        get_header();
+
+        if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+
+            <article class="container page-content">
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article>
+
+            <?php endwhile;
+
+        else :
+            echo '<p>No content found</p>';
+
+        endif;
+
+        get_footer();
+
+    ?>
+
+<?php get_footer(); ?>

cea73c0842fa8dd0b33e618c769c8599dab7f57f
Ivan Bumbak <bumbak.ivan@gmail.com>
2020-07-08 09:48:35 +0200

    styling pages

--- page-partners.php
+++ page-partners.php
@@ -5,63 +5,63 @@ get_header(); ?>
 <div class="container">
     <div class="wrapper-partners">
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://deltron.hr/" target="_blank">Deltron</a>
+            <p>Vukovarska 148<br>21000 Split</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Corona Copy d.o.o.</a>
-            <p>Cesta Dr. Franje Tuđmana 332, 21212 Kaštel Sućurac</p>
+            <a href="http://www.frigo.com.hr/" target="_blank">Frigo d.o.o.</a>
+            <p>Sunekova 182 Dubrava<br>10040 ZAGREB</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Frigo d.o.o.</a>
-            <p>Sunekova 182 Dubrava, 10040 ZAGREB</p>
+            <a href="https://www.corona-copy.hr/" target="_blank">Corona Copy d.o.o.</a>
+            <p>Cesta Dr. Franje Tuđmana 332<br>21212 Kaštel Sućurac</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="http://gg-grupa.eu/" target="_blank">Gastrobit Grc Grupa d.o.o.</a>
+            <p>Zagrebačka 145b<br>10370 Dugo Selo (Zagreb)</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.also.com/" target="_blank">ALSO d.o.o.</a>
+            <p>Av. Većeslava Holjevca 40<br>10000 Zagreb</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.lost.hr/" target="_blank">LOST d.o.o.</a>
+            <p>Kreše Golika 7<br>10000 Zagreb</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.makromikrogrupa.hr/" target="_blank">Makromikro d.o.o.</a>
+            <p>Bani 73B<br>10010 Zagreb - Buzin</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.msan.hr/" target="_blank">M-San Grupa d.d.</a>
+            <p>Buzinski prilaz 10<br>10010 Zagreb</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.digitron.hr/" target="_blank">Digitron d.o.o.</a>
+            <p>Digitronska 33<br>52460 Buje</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.se-mark.hr/" target="_blank">SE-Mark d.o.o.</a>
+            <p>Bjelovarska 36a<br>10360 Sesvete</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="https://www.eurotrade.hr/" target="_blank">Eurotrade d.o.o.</a>
+            <p>Naselje Gripole 53/C<br>52210 Rovinj</p>
         </div>
 
         <div class="section-quarter-partners">
-            <a href="">Gastrobit Grc Grupa d.o.o.</a>
-            <p>Zagrebačka 145b, 10370 Dugo Selo (Zagreb)</p>
+            <a href="http://www.feniks-info.hr/" target="_blank">Feniks-info d.o.o.</a>
+            <p>Maksimirska 132<br>10000 Zagreb</p>
         </div>
     </div>
 </div>

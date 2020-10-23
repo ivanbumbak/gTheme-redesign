02954a77aceb91a20280b01502dbaece5a9e2b15
Ivan Bumbak <bumbak.ivan@gmail.com>
2020-10-23 14:07:37 +0200

    show on scroll js

--- functions.php
+++ functions.php
@@ -5,14 +5,19 @@ function gTheme_resources() {
     wp_enqueue_style('style', get_stylesheet_uri());
 
     //Custom JS
-    wp_register_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'));
-    wp_enqueue_script('script');
+    wp_register_script( 'script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ) );    
+	wp_enqueue_script('script');
 }
 add_action('wp_enqueue_scripts', 'gTheme_resources');
 
-//Navigation menus
-register_nav_menus(array(
-    'primary' => __('Main Menu'),
-));
+//Theme Setup
+function gTheme_setup() {
+    //Navigation menus
+    register_nav_menus(array(
+        'primary' => __('Main Menu'),
+    ));
+
+}
+add_action('after_setup_theme', 'gTheme_setup');
 
 ?>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+//Show Nutshell
+const nutshell = document.getElementsByClassName('nutshell-third');
+    setTimeout(() => {
+        for(let n = 0; n < nutshell.length; n++) {
+            nutshell[n].classList.add('show');
+        }
+    }, 1100);
+
+//Google Map
 function mapLocation() {
     let location = {lat: 43.766012, lng: 15.903020};
     let map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: location});
     let marker = new google.maps.Marker({position: location, map: map});
+    console.log('Map: ', map)
 }

616031c5a6cddc7e090a1fd1ddf7fd0659da52b6
Ivan Bumbak <bumbak.ivan@gmail.com>
2020-11-05 12:25:07 +0100

    respond-page

--- page-thank-you.php
+++ page-thank-you.php
@@ -4,8 +4,8 @@ get_header(); ?>
 
 <div class="container thank-you">
     <div class="wrapper-thank-you">
-        <i class="far fa-check-circle"></i>
+        <i class="fal fa-check-circle"></i>
         <h1>Hvala! Vaš upit je uspješno poslan!</h1>
-        <a href="<?php echo home_url(); ?>"><i class="far fa-arrow-alt-circle-left"></i>Povratak na početnu stranicu.</a>
+        <a href="<?php echo home_url(); ?>"><i class="fal fa-backward"></i>Povratak na početnu stranicu</a>
     </div>
 </div>

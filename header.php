f2f5e0ed85929c95ee505e8c0c24a7467c6ac7d6
ivanbumbak <bumbak.ivab@gmail.com>
2021-05-06 17:38:40 +0200

    header update

--- header.php
+++ header.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
+        <meta name="alexaVerifyID" content="XtfZVf-q78GnEI_taqRy4NABkq0"/>
+		<meta name="google-site-verification" content="GiIU8RuEFeXKL8fxOJ96i2i27ck8tXl725Pw_vDg0Vc" />
+		<meta name="keywords" content="gambi, gambi doo, gambi šibenik, šibenik, fiskalne blagajne, fiskalizacija, klimatizacija, klima uređaj, samsung, gree, rashladni sustavi, vrf sustavi, instalacije, servis računala, uredski materijal">
         <meta charset="<?php bloginfo('charset'); ?>">
+        <link rel="canonical" href="http://www.gambi.hr" /> 
+		<link rel="alternate" href="http://www.gambi.hr/" hreflang="hr-hr" />
         <meta name="viewport" content="width=device-width">
         <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans&display=swap" rel="stylesheet">
         <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous"> -->

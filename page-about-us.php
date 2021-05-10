0765647d82412ba7186631bd017a93392489d31c
ivanbumbak <bumbak.ivab@gmail.com>
2021-05-10 08:40:40 +0200

    about us layout update

--- page-about-us.php
+++ page-about-us.php
@@ -3,24 +3,8 @@
 get_header(); ?>
 
 <div class="portfolio">
-    <div class="about-us-background">
-    </div>
-
     <div class="about-us">
-        <?php if (have_posts()) :
-            while (have_posts()) : the_post(); ?>
-
-            <article class="container page-content">
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article>
-
-            <?php endwhile;
-
-        else :
-            echo '<p>No content found</p>';
-
-        endif; ?>
+        <h1>O nama</h1>
     </div>
 </div>
 
